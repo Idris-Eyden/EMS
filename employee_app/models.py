8fa822944145414757113b8dca7818766e7ce7a4
Idris-Eyden <abdiwahab14265 @gmail.com>
2025-11-07 23:47:14 +0300

    updated views

--- employee_app/models.py
+++ employee_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+	employee_id = models.CharField(max_length=100, null=True)
+	employee_name = models.CharField(max_length=100, null=True)
+	employee_email = models.EmailField()
+	employee_contact = models.CharField(max_length=20, null=True)
+
+
+	def __str__(self):
+		return self.employee_name
--- employee_app/views.py
+++ employee_app/views.py
@@ -1,4 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import EmployeeForm
+from . models import Employee
+from django.contrib import messages
+
+
 
 def home(request):
 	return render(request, 'employee_app/home.html', {})
+def create_employee(request):
+	if request.method == 'POST':
+		form = EmployeeForm(request.POST)
+
+		if  form .is_valid():
+			form.save()
+			return redirect('home')
+	else:
+		form = EmployeeForm()
+	return render(request, 'employee_app/create_employee.html', {
+		'form':form
+		})
+
+def employee_list(request):
+	 employees = Employee.objects.all()
+	 return render(request, 'create_employee/list.html', {
+	 	'employees':employees
+	 	})
+
+
+def update_employee(request, id):
+	employee = Employee.objects.get(pk=id)
+	if request.method == 'POST':
+		form = EmployeeForm(request.POST, instance=employee)
+
+		if form.is_valid():
+			form.save()
+			messages.success(request, f'Employee updated successfully')
+			return redirect('home')
+	else:
+		form = EmployeeForm(instance=employee)
+
+	return render(request, 'employee_app/update.html', {
+		'form':form
+		})
+def delete_employee(request, id):
+	employee = Employee.objects.get(pk=id)
+	if request.method == 'POST':
+		employee.delete()
+	return redirect('list')
