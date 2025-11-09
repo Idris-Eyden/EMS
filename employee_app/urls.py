e2ca1b6a8d8a341622437419d801fe8b6df452d3
Idris-Eyden <abdiwahab14265 @gmail.com>
2025-11-09 23:06:14 +0300

    updated urls

--- employee_app/urls.py
+++ employee_app/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
 	path('', views.home, name='home'),
+	path('list', views.employee_list, name='list'),
+	path('create', views.create_employee, name='create'),
+	path('update/<int:id>/', views.update_employee, name='update'),
+	path('delete/<int:id>', views.delete_employee, name='delete'),
 
 ]
